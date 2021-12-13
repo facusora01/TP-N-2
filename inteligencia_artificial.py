a4bfcc6169ea6176a5dca386bcafec308619579c
Valentina Lanzillotta <vlanzillotta@fi.uba.ar>
2021-12-13 17:17:01 -0300

    Quité funciones repetidas y arregle el codigo de ia; renombre el Ej. 7

--- inteligencia_artificial.py
+++ inteligencia_artificial.py
@@ -2,39 +2,35 @@ import cv2
 import numpy as np
 import os
 
-def load_yolo():
+def cargar_red() -> tuple:
 	net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 	classes: list = []
 	with open("coco.names", "r") as f:
 		classes = [line.strip() for line in f.readlines()]
 	layers_names = net.getLayerNames()
 	output_layers = [layers_names[i-1] for i in net.getUnconnectedOutLayers()]
-	colors = np.random.uniform(0, 255, size=(len(classes), 3))
-	return net, classes, colors, output_layers
+	return net, classes, output_layers
 
-def load_image(img_path):
+def cargar_imagen(img_path) -> tuple:
 	img = cv2.imread(img_path)
 	img = cv2.resize(img, None, fx=0.4, fy=0.4)
 	height, width, channels = img.shape
-	return img, height, width, channels
+	return img, height, width
 
-def detect_objects(img, net, outputLayers):			
+def detectar_objetos(img, net, outputLayers) -> tuple:			
 	blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
 	net.setInput(blob)
 	outputs = net.forward(outputLayers)
-	return blob, outputs
+	return outputs
 
-def draw_labels(boxes, confs, colors, class_ids, classes, img): 
+def etiqueta(boxes, confs, class_ids, classes) -> str:
 	indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
-	font = cv2.FONT_HERSHEY_PLAIN
 	for i in range(len(boxes)):
 		if i in indexes:
-			x, y, w, h = boxes[i]
 			label = str(classes[class_ids[i]])
-			color = colors[i]
 	return label
 
-def get_box_dimensions(outputs, height, width):
+def dimensiones(outputs, height, width) -> tuple:
 	boxes: list = []
 	confs: list = []
 	class_ids: list = []
@@ -55,125 +51,71 @@ def get_box_dimensions(outputs, height, width):
 				class_ids.append(class_id)
 	return boxes, confs, class_ids
 
-def image_detect(img_path): 
-	model, classes, colors, output_layers = load_yolo()
-	image, height, width, channels = load_image(img_path)
-	blob, outputs = detect_objects(image, model, output_layers)
-	boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
-	label: str = draw_labels(boxes, confs, colors, class_ids, classes, image)
+def image_detect(img_path) -> str:
+	model, classes, output_layers = cargar_red()
+	image, height, width = cargar_imagen(img_path)
+	outputs = detectar_objetos(image, model, output_layers)
+	boxes, confs, class_ids = dimensiones(outputs, height, width)
+	label: str = etiqueta(boxes, confs, class_ids, classes)
 	return label
-	# key = 0
-	# while key != 27:                        # esto no se para que es pero anda bien sin ponerlo, si no se queda colgada la terminal
-	# 	key = cv2.waitKey(1)
 
-
-def color_rojo(path_imagen) -> bool:
+def colores(path_imagen, lower_array, upper_array) -> bool:
 	img = cv2.imread(path_imagen)
 	img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	lower = np.array([0, 50, 70])
-	upper = np.array([9, 255, 255])
+	lower = np.array(lower_array)
+	upper = np.array(upper_array)
 	mask = cv2.inRange(img_HSV, lower, upper)
 	img_result = cv2.bitwise_and(img, img, mask=mask)
-	cantidad_rojo: float = np.sum(img_result) / np.sum(img_HSV)
-	
-	if cantidad_rojo > 0.001:
+	cantidad: float = np.sum(img_result) / np.sum(img_HSV)
+
+	if cantidad > 0.001:
 		return True
 	else:
 		return False
 
-def color_amarillo(path_imagen) -> bool:
-	img = cv2.imread(path_imagen)
-	img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	lower = np.array([20, 100, 100])
-	upper = np.array([30, 255, 255])
-	mask = cv2.inRange(img_HSV, lower, upper)
-	img_result = cv2.bitwise_and(img, img, mask=mask)
-	cantidad_amarillo: float = np.sum(img_result) / np.sum(img_HSV)
+def color_predominante(path_imagen, objeto, dicc_colores_botellas, dicc_colores_vasos) -> None:
+	rojo: bool = colores(path_imagen, [0, 50, 70], [9, 255, 255])
+	amarillo: bool = colores(path_imagen, [20, 100, 100], [30, 255, 255])
+	azul: bool = colores(path_imagen, [90, 50, 70], [128, 255, 255])
+	verde: bool = colores(path_imagen, [36, 50, 70], [89, 255, 255])
+	negro: bool = colores(path_imagen, [0, 0, 0], [180, 255, 30])
+	lista_colores: list = [rojo, amarillo, azul, verde, negro]
+	for color in range(len(lista_colores)):
+		if lista_colores[color] == True:
+			if color == 0 and (objeto == "bottle"):
+				dicc_colores_botellas["Rojo"] += 1
+			elif color == 1 and (objeto == "bottle"):
+				dicc_colores_botellas["Amarillo"] += 1
+			elif color == 2:
+				if objeto == "bottle":
+					dicc_colores_botellas["Azul"] += 1
+				elif objeto == "cup":
+					dicc_colores_vasos["Azul"] += 1
+			elif color == 3 and (objeto == "bottle"):
+				dicc_colores_botellas["Verde"] += 1
+			elif color == 4:
+				if objeto == "bottle":
+					dicc_colores_botellas["Negro"] += 1
+				elif objeto == "cup":
+					dicc_colores_vasos["Negro"] += 1
 
-	if cantidad_amarillo > 0.001:
-		return True
-	else:
-		return False
-
-def color_azul(path_imagen) -> bool:
-	img = cv2.imread(path_imagen)
-	img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	lower = np.array([90, 50, 70])
-	upper = np.array([128, 255, 255])
-	mask = cv2.inRange(img_HSV, lower, upper)
-	img_result = cv2.bitwise_and(img, img, mask=mask)
-	cantidad_azul: float = np.sum(img_result) / np.sum(img_HSV)
-
-	if cantidad_azul > 0.001:
-		return True
-	else:
-		return False
-
-def color_verde(path_imagen) -> bool:
-	img = cv2.imread(path_imagen)
-	img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	lower = np.array([36, 50, 70])
-	upper = np.array([89, 255, 255])
-	mask = cv2.inRange(img_HSV, lower, upper)
-	img_result = cv2.bitwise_and(img, img, mask=mask)
-	cantidad_verde: float = np.sum(img_result) / np.sum(img_HSV)
-
-	if cantidad_verde > 0.001:
-		return True
-	else:
-		return False
-
-def color_negro(path_imagen) -> bool:
-	img = cv2.imread(path_imagen)
-	img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	lower = np.array([0, 0, 0])
-	upper = np.array([180, 255, 30])
-	mask = cv2.inRange(img_HSV, lower, upper)
-	img_result = cv2.bitwise_and(img, img, mask=mask)
-	cantidad_negro: float = np.sum(img_result) / np.sum(img_HSV)
-
-	if cantidad_negro > 0.001:
-		return True
-	else:
-		return False
-
-def color_predominante(path_imagen, objeto, dicc_colores_botellas, dicc_colores_vasos):
-	if (color_amarillo(path_imagen) == True) and (objeto == "bottle"):
-		dicc_colores_botellas["Amarillo"] += 1
-	elif (color_rojo(path_imagen) == True) and (objeto == "bottle"):
-		dicc_colores_botellas["Rojo"] += 1
-	elif (color_azul(path_imagen)) == True:
-		if objeto == "bottle":			
-			dicc_colores_botellas["Azul"] += 1
-		elif objeto == "cup":
-			dicc_colores_vasos["Azul"] += 1
-	elif (color_negro(path_imagen) == True):
-		if objeto == "bottle":			
-			dicc_colores_botellas["Negro"] += 1
-		elif objeto == "cup":
-			dicc_colores_vasos["Negro"] += 1
-	elif (color_verde(path_imagen) == True) and (objeto == "bottle"):
-		dicc_colores_botellas["Verde"] += 1
-	else:
-		return "None"
-
-def main():
+def main() -> dict:
 	dicc_objetos: dict = {}
 	dicc_colores_botellas: dict = {"Amarillo": 0, "Rojo": 0, "Azul": 0, "Negro": 0, "Verde": 0}
-	dicc_colores_vasos: dict = {"Negro": 0, "Azul": 0} # hago un contador de los colores de vasos y botellas
+	dicc_colores_vasos: dict = {"Negro": 0, "Azul": 0}
 	path: str = os.getcwd() + "\\Lote0001"
 	nombre_imagenes: list = os.listdir(path)
 	for imagen in nombre_imagenes:
 		path_imagen: str = path + "\\" + imagen
-		objeto: str = image_detect(path_imagen)	
+		objeto: str = image_detect(path_imagen)
 		if objeto != "bottle" and objeto != "cup":
 			print("PROCESO DETENIDO, se reanuda en 1 minuto")
-		color: str = color_predominante(path_imagen, objeto, dicc_colores_botellas, dicc_colores_vasos)
+		color_predominante(path_imagen, objeto, dicc_colores_botellas, dicc_colores_vasos)
 		if objeto == "bottle":
 			dicc_objetos["Botellas"] = dicc_colores_botellas
 		elif objeto == "cup":
 			dicc_objetos["Vasos"] = dicc_colores_vasos
 
-	# print(dicc_objetos)
+	return dicc_objetos
 
 main()

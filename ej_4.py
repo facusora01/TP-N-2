4d445f921a1bc950eda5b8f73a4f9d5218aaba4d
Facusora <facusora@hotmail.com>
2021-12-21 20:56:29 -0300

    Borré cosas innecesarias

--- ej_4.py
+++ ej_4.py
@@ -5,13 +5,11 @@ from os import close
 def leer_csv()-> tuple:
 
     #POST: Lee el .csv, y devuelve la informacion que traía dentro
+
     with open(r"pedidos.csv") as archivo:
         informacion: csv = csv.reader(archivo)
         pedidos_temporal: list = list(informacion)
-    
-    # lectura: csv = csv.reader(open(r"pedidos.csv"), delimiter=",")
-    # ordenar_info: list = sorted(lectura, reverse = True)
-    # close
+
     return pedidos_temporal
 
 def contador_vasos_botellas(ciudad, cod_articulo, cantidad, vasos, botellas, ciudad_destino)-> tuple:
@@ -34,6 +32,7 @@ def decisicion_modificacion(total, lectura_csv, vasos, botellas, ciudad_destino)
     #PRE: Recibe como parametros: una lista vacia, la info del .csv, y 3 listas vacias
     #POST: Se crea un for simplemente para remover una lista vacia, luego se crea otro for donde se le preg. al usuario si quiere cambiar la cant. del pedido de cada uno
     # devolviendo una lista con todas las ciudades de los pedidos
+    
     for fila in lectura_csv:
         if [] in lectura_csv: 
             lectura_csv.remove([])
@@ -44,7 +43,7 @@ def decisicion_modificacion(total, lectura_csv, vasos, botellas, ciudad_destino)
 
         if not 'Cantidad' in fila[7]:
             desicion: str = input("\n\nDesea modificar la cantidad del pedido?\n(Presione 'y', si asi lo desea, sino presione 'Enter): ")
-
+            
             if desicion == 'y':
                 cantidad: int = int(input("Escriba la nueva cantidad: "))
                 print("")
@@ -66,10 +65,8 @@ def decisicion_modificacion(total, lectura_csv, vasos, botellas, ciudad_destino)
 
 def imprimir_tablero(lectura_csv)-> None:
 
-
-    # for fila in lectura_csv:
-    #     for item in fila:
-    #         print(item, end=" ")
+    #PRE: Recibe como parametros los datos del .csv
+    #POST: Se encarga de imprimir el tablero. No devuelve nada
 
     for fila in lectura_csv:
         if not 'Nro. Pedidio' in fila[0]:
@@ -79,74 +76,6 @@ def imprimir_tablero(lectura_csv)-> None:
         else:
             print(' '.join(str(x) for x in fila))
             
-
-
-
-    #PRE: Recibe como parametro: Los datos que traía el .csv
-    #POST:  se hace un for con varios if dentro, los cuales se encargan simplemente de que la impresión del tablero se vea decente. No devuelve nada
-
-    # for numero_pedido, fecha, cliente, ciudad, provincia, cod_articulo, color, cantidad, descuento in lectura_csv:
-  
-    #     if numero_pedido != 'Nro. Pedidio':
-
-    #         numero_pedido: str = numero_pedido.center(len(numero_pedido)+5)
-        
-    #     if fecha != ' Fecha':
-
-    #         fecha: str = fecha.rjust(len(fecha)+6)
-        
-    #     if cliente != ' Cliente':
-
-    #         cliente: str = cliente.center(len(cliente))
-        
-    #     if ciudad == ' Ciudad':
-
-    #         ciudad: str = ciudad.rjust(len(ciudad)+1)
-        
-    #     if ciudad != ' Ciudad':
-
-    #         if len(cliente) < 16:
-
-    #             ciudad: str = ciudad.rjust(len(ciudad)+8)
-
-    #     if provincia == ' Provincia':
-
-    #         provincia: str = provincia.rjust(len(provincia)+1)
-
-    #     if provincia != ' Provincia':
-
-    #         if len(cliente) > 16:
-
-    #             provincia: str = provincia.rjust(len(provincia)+8)
-            
-    #         elif len(provincia) >= 12:
-
-    #             provincia: str = provincia.rjust(len(provincia)+8)
-
-    #     if cod_articulo != ' Cod. Artículo':
-
-    #         cod_articulo: str = cod_articulo.center(len(cod_articulo)+6)
-
-    #     if color != ' Color':
-
-    #         color: str = color.rjust(len(color)+9)
-
-    #     if cantidad != ' Cantidad':
-
-    #         if color == '         Amarillo' or color == '         Negro':
-
-    #             cantidad: str = cantidad.rjust(len(cantidad)+2)
-
-    #         else:
-    #             cantidad: str = cantidad.rjust(len(cantidad)+10)
-
-
-    #     if descuento != ' Descuento':
-
-    #         descuento: str = descuento.rjust(len(descuento)+10)
-
-    #     print(numero_pedido,'\t', fecha,"\t", cliente,'\t', ciudad,"\t", provincia,"\t",cod_articulo,"\t", color,"\t", cantidad,'\t', descuento)
-
 
 
 def direccion_de_pedidos(ciudades_de_pedidos)-> list:
@@ -188,7 +117,7 @@ def main() -> None:
     imprimir_tablero(lectura_csv)
 
     
-    print("\nfueron", len(lectura_csv[1])-1, "Pedidos")
+    print("\nfueron", len(lectura_csv)-1, "Pedidos")
 
     print(f"fueron", total[0], "vasos, y", total[1], "botellas")
 
